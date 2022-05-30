6a49493ee0ab69866290b194e40b077afd7a6505
uoas6uoaf <bbh3032@naver.com>
2022-05-30 22:15:56 +0900

    0527 ex

---
+++ HTML5/script.js
@@ -0,0 +1,14 @@
+id = prompt("아이디 입력");     //내장함수
+console.log('id: ',id);
+
+// if(id =="admin") {
+//     password = prompt("비밀번호 입력");
+
+//     if (password ==="123456") {
+//         location.href = "http://www.naver.com";
+//     }else {
+//         location.href = "http://www.daum.net";
+//     }
+// }else {
+//     location.href = "http://www.google.co.kr";
+// }
